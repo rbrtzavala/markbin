2a34c6f5bb8a5b633f418e4e4058021182072a86
RAZ <rbrtzavala@gmail.com>
2017-05-24 10:25:20 -0600

    Created BinsViewer component to render the code editor output with the markdown package

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
+import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends Component {
+
   render() {
-    console.log(this.props.bin);
+    if (!this.props.bin) { return <div>Loading...</div> };
+
     return (
-      <div>BinsMain</div>
+      <div>
+        <BinsEditor bin={this.props.bin} />
+        <BinsViewer bin={this.props.bin} />
+      </div>
     );
   }
 }
